7b730efc3a76719d34bed258b6b08b9259c0711b
Alberto Sierra <alberto.sierra@murciaeduca.es>
2024-04-09 16:52:09 +0100

    updated file exercises/fd6f904e-e39b-4790-8336-b37ab59baf99/solutions/2ae64389-f149-4f33-aa5f-89efa2e04f72/solution.sql

--- exercises/fd6f904e-e39b-4790-8336-b37ab59baf99/solutions/2ae64389-f149-4f33-aa5f-89efa2e04f72/solution.sql
+++ exercises/fd6f904e-e39b-4790-8336-b37ab59baf99/solutions/2ae64389-f149-4f33-aa5f-89efa2e04f72/solution.sql
@@ -1,17 +1,1 @@
-CREATE OR REPLACE FUNCTION get_empleados_count(_department_id int)
-	RETURNS int
-	LANGUAGE plpgsql
-AS $function$
-	DECLARE 
-		contador_empleados int;
-	BEGIN
-		SELECT COUNT(*)
-		into
-			contador_empleados
-		from
-			employees
-		where
-			DEPARTMENT_ID = _department_id;
-		return contador_empleados;
-	END;
-$function$
+select get_empleados_count(30) as dep_30, get_empleados_count(90) as dep_90;
