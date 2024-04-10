bd2e191821e899f03d9010db6a6d6cbbf2b5464a
Alberto Sierra <alberto.sierra@murciaeduca.es>
2024-04-10 17:18:41 +0100

    updated file exercises/23688662-3533-4e2f-b7d0-818d7c506a44/solutions/16289655-e259-4af5-bb70-f08eb5b35595/solution.sql

--- exercises/23688662-3533-4e2f-b7d0-818d7c506a44/solutions/16289655-e259-4af5-bb70-f08eb5b35595/solution.sql
+++ exercises/23688662-3533-4e2f-b7d0-818d7c506a44/solutions/16289655-e259-4af5-bb70-f08eb5b35595/solution.sql
@@ -1,17 +1,29 @@
-CREATE OR REPLACE FUNCTION get_empleados_count(_department_id int)
-	RETURNS int
+CREATE OR REPLACE FUNCTION sustituto_por_antiguedad(_department_id int)
+	RETURNS varchar(50)
 	LANGUAGE plpgsql
 AS $function$
 	DECLARE 
-		contador_empleados int;
+		_manager_id int;
+		valor_devuelto varchar(30);
 	BEGIN
-		SELECT COUNT(*)
-		into
-			contador_empleados
-		from
-			employees
-		where
-			DEPARTMENT_ID = _department_id;
-		return contador_empleados;
+		select manager_id
+			into _manager_id
+			from departments
+			where department_id = _department_id;
+
+		select employee_id || ' - ' || last_name || ', ' || first_name
+			into valor_devuelto
+			from EMPLOYEES E
+			where department_id = _department_id
+				and HIRE_DATE <= all (
+					select hire_date
+					from employees
+					where DEPARTMENT_ID = _department_id and EMPLOYEE_ID != _manager_id
+				)
+				and EMPLOYEE_ID != _manager_id;
+			
+		if valor_devuelto is null then  valor_devuelto := 'Sin sustituto'; end if;
+		return valor_devuelto;
 	END;
 $function$
+;
