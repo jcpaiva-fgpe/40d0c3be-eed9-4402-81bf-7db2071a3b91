ffa13ed454c29f3f442c04c02c6d268c0dbf571c
Alberto Sierra <alberto.sierra@murciaeduca.es>
2024-04-10 16:23:37 +0100

    updated file exercises/a2e2c5a2-7224-41ea-a936-e53f9b02f645/solutions/41cd1de3-65bb-4f70-bbe5-463b40dff2ca/solution.sql

--- exercises/a2e2c5a2-7224-41ea-a936-e53f9b02f645/solutions/41cd1de3-65bb-4f70-bbe5-463b40dff2ca/solution.sql
+++ exercises/a2e2c5a2-7224-41ea-a936-e53f9b02f645/solutions/41cd1de3-65bb-4f70-bbe5-463b40dff2ca/solution.sql
@@ -1,17 +1,26 @@
-CREATE OR REPLACE FUNCTION get_empleados_count(_department_id int)
-	RETURNS int
+CREATE OR REPLACE FUNCTION get_subordinados_departamento(_department_id int)
+	RETURNS varchar(30)
 	LANGUAGE plpgsql
 AS $function$
 	DECLARE 
 		contador_empleados int;
+		valor_devuelto varchar(30);
 	BEGIN
 		SELECT COUNT(*)
 		into
 			contador_empleados
 		from
-			employees
+			departments join employees using (department_id)
 		where
 			DEPARTMENT_ID = _department_id;
-		return contador_empleados;
+		
+		case contador_empleados
+			when 0 then valor_devuelto := 'Sin empleados';
+			when 1 then valor_devuelto := 'Solo el jefe';
+			else valor_devuelto := contador_empleados - 1;
+		end case;
+	
+		return valor_devuelto;
 	END;
 $function$
+;
