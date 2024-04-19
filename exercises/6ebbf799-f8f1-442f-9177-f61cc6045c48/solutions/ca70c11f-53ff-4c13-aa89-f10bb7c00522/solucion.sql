d082629fe96727a88e0f712a0cbc0307da94de43
Andrew Nightingale <andrewpeter.nightingale@murciaeduca.es>
2024-04-19 12:40:46 +0100

    created file exercises/6ebbf799-f8f1-442f-9177-f61cc6045c48/solutions/ca70c11f-53ff-4c13-aa89-f10bb7c00522/solucion.sql

---
+++ exercises/6ebbf799-f8f1-442f-9177-f61cc6045c48/solutions/ca70c11f-53ff-4c13-aa89-f10bb7c00522/solucion.sql
@@ -0,0 +1,26 @@
+CREATE TABLE auditaremple(operacion VARCHAR(200));
+
+CREATE OR REPLACE FUNCTION auditar_emp()
+RETURNS TRIGGER
+LANGUAGE PLPGSQL
+AS
+$function$
+BEGIN
+	IF TG_OP = 'DELETE' THEN 
+		INSERT INTO auditaremple
+		   VALUES (TO_CHAR(CURRENT_DATE, 'DD/MM/YY*HH24:MI*') || 
+			old.emp_no || '*' || old.apellido || '* BORRADO ');
+	ELSIF TG_OP = 'INSERT' THEN
+		INSERT INTO auditaremple
+		   VALUES (TO_CHAR(SYSDATE, 'DD/MM/YY*HH24:MI*') || 
+			new.emp_no || '*' || new.apellido || '* INSERCIÓN ');
+	END IF;
+	RETURN NEW;
+END;
+$function$
+
+CREATE OR REPLACE TRIGGER auditar_act_emp
+	BEFORE INSERT OR DELETE
+	ON EMPLE
+	FOR EACH ROW
+EXECUTE PROCEDURE auditar_emp();
