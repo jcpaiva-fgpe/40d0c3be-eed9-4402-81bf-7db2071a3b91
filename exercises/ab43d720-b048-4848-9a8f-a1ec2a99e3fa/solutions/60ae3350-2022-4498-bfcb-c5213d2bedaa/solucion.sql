0f6a9ba5ddedc9090977378cf77e89016ed8aff7
Andrew Nightingale <andrewpeter.nightingale@murciaeduca.es>
2024-04-20 10:01:24 +0100

    created file exercises/ab43d720-b048-4848-9a8f-a1ec2a99e3fa/solutions/60ae3350-2022-4498-bfcb-c5213d2bedaa/solucion.sql

---
+++ exercises/ab43d720-b048-4848-9a8f-a1ec2a99e3fa/solutions/60ae3350-2022-4498-bfcb-c5213d2bedaa/solucion.sql
@@ -0,0 +1,33 @@
+CREATE OR REPLACE FUNCTION gestionar_dept()
+RETURNS TRIGGER
+LANGUAGE PLPGSQL
+AS
+$function$
+DECLARE
+	existe_dept_nombre int;
+	nuevo_dept_id int;
+BEGIN
+	IF TG_OP = 'DELETE' THEN 
+		UPDATE EMPLE SET dept_no = -1 where dept_no = OLD.dept_no;
+	ELSIF TG_OP = 'INSERT' THEN
+		SELECT count(*) INTO existe_dept_nombre
+		from depart
+		WHERE dnombre = NEW.dnombre;
+		IF existe_dept_nombre > 0 THEN
+			raise 'Ya existe un departamento con ese nombre';
+		ELSE
+			SELECT MAX(dept_no)+10
+			INTO nuevo_dept_id 
+			FROM depart;
+			NEW.dept_no := nuevo_dept_id;
+		END IF;
+	END IF;
+	RETURN NEW;
+END;
+$function$;
+
+CREATE OR REPLACE TRIGGER gestionar_departamento
+	BEFORE INSERT OR DELETE
+	ON DEPART
+	FOR EACH ROW
+EXECUTE PROCEDURE gestionar_dept();
