cd44090dba520bc6a83b23eb05ef7decdb227c23
Andrew Nightingale <andrewpeter.nightingale@murciaeduca.es>
2024-04-21 11:17:29 +0100

    created file exercises/19bed022-4940-48c6-b549-819e44d8ff9e/solutions/35b608e6-dce8-4428-941b-18819657d9d2/solucion.sql

---
+++ exercises/19bed022-4940-48c6-b549-819e44d8ff9e/solutions/35b608e6-dce8-4428-941b-18819657d9d2/solucion.sql
@@ -0,0 +1,15 @@
+CREATE or replace FUNCTION agregar_dominio_email_empleado()
+RETURNS TRIGGER
+LANGUAGE plpgsql
+AS $$
+BEGIN
+   new.email = CONCAT(new.email, '@gmail.es');
+   return new;
+END;
+$$;
+
+CREATE TRIGGER nuevo_empleado
+BEFORE INSERT
+ON employees
+FOR EACH ROW
+EXECUTE PROCEDURE agregar_dominio_email_empleado();
