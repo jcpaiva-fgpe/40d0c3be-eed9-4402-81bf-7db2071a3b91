8899533b8c912e9cefe9190d0bdfe02ba72f5b54
Andrew Nightingale <andrewpeter.nightingale@murciaeduca.es>
2024-04-21 11:31:26 +0100

    created file exercises/141fe5b4-6964-4373-9195-91978fe31def/solutions/3df6bc73-fd6d-4ca9-b0b7-6b46fda01e5b/solucion.sql

---
+++ exercises/141fe5b4-6964-4373-9195-91978fe31def/solutions/3df6bc73-fd6d-4ca9-b0b7-6b46fda01e5b/solucion.sql
@@ -0,0 +1,24 @@
+CREATE or replace FUNCTION comprueba_departamento()
+RETURNS TRIGGER
+LANGUAGE plpgsql
+AS $$
+declare 
+	departamento_empleado int;
+BEGIN
+   select department_id 
+   into departamento_empleado
+   from employees where employee_id = new.employee_id;
+  
+  if departamento_empleado <> new.department_id then
+  	new.department_id = departamento_empleado;
+  end if;
+  
+  return new;
+END;
+$$;
+
+create or replace TRIGGER comprobar_departamento_historico
+before INSERT
+ON job_history
+FOR EACH ROW
+EXECUTE PROCEDURE comprueba_departamento();
