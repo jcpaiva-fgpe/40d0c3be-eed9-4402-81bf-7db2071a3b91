2231c4a6927595429ce7772b62a4704dc095d61f
Andrew Nightingale <andrewpeter.nightingale@murciaeduca.es>
2024-04-21 11:17:20 +0100

    created file exercises/19bed022-4940-48c6-b549-819e44d8ff9e/tests/668ec358-ef92-480a-a559-2dac5fc85efd/in.sql

---
+++ exercises/19bed022-4940-48c6-b549-819e44d8ff9e/tests/668ec358-ef92-480a-a559-2dac5fc85efd/in.sql
@@ -0,0 +1,4 @@
+insert into employees(employee_id, first_name, last_name, email, phone_number, hire_date, job_id, salary, manager_id, department_id)
+values (1, 'Andrew', 'Nightingale', 'cats.andy', '635960194', '2024-01-01', 'IT_PROG', 18000, 100, 90);
+
+select * from employees where employee_id = 1;
