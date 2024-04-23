f3e4a15d69133c9366d2e38ec474dcc4b3277ea3
Andrew Nightingale <andrewpeter.nightingale@murciaeduca.es>
2024-04-23 10:53:06 +0100

    updated file exercises/6ebbf799-f8f1-442f-9177-f61cc6045c48/libraries/3f12ce8a-1bc3-41a9-b0ee-2fec05d41d3f/empleadosDepartamentosPostgreSQL.sql

--- exercises/6ebbf799-f8f1-442f-9177-f61cc6045c48/libraries/3f12ce8a-1bc3-41a9-b0ee-2fec05d41d3f/empleadosDepartamentosPostgreSQL.sql
+++ exercises/6ebbf799-f8f1-442f-9177-f61cc6045c48/libraries/3f12ce8a-1bc3-41a9-b0ee-2fec05d41d3f/empleadosDepartamentosPostgreSQL.sql
@@ -1,7 +1,8 @@
 CREATE TABLE DEPART (
- DEPT_NO  numeric(2) PRIMARY KEY,
- DNOMBRE  varchar(14), 
- LOC      varchar(14) ) ;
+	 DEPT_NO  numeric(2) PRIMARY KEY,
+	 DNOMBRE  varchar(14), 
+	 LOC      varchar(14) 
+);
 
 INSERT INTO DEPART VALUES (10,'CONTABILIDAD','SEVILLA');
 INSERT INTO DEPART VALUES (20,'INVESTIGACION','MADRID');
@@ -9,14 +10,15 @@ INSERT INTO DEPART VALUES (30,'VENTAS','BARCELONA');
 INSERT INTO DEPART VALUES (40,'PRODUCCION','BILBAO');
 
 CREATE TABLE EMPLE (
- EMP_NO    numeric(4) PRIMARY KEY,
- APELLIDO  varchar(10)  ,
- OFICIO    varchar(10)  ,
- DIR       numeric(4) ,
- FECHA_ALT DATE      ,
- SALARIO   numeric(7),
- COMISION  numeric(7),
- DEPT_NO   numeric(2) NOT NULL) ;
+	 EMP_NO    numeric(4) PRIMARY KEY,
+	 APELLIDO  varchar(10)  ,
+	 OFICIO    varchar(10)  ,
+	 DIR       numeric(4) ,
+	 FECHA_ALT DATE      ,
+	 SALARIO   numeric(7),
+	 COMISION  numeric(7),
+	 DEPT_NO   numeric(2) NOT NULL
+);
 
 INSERT INTO EMPLE VALUES (7369, 'SANCHEZ', 'EMPLEADO', 7902, '1990-12-17', 1040, NULL, 20);
 INSERT INTO EMPLE VALUES (7499, 'ARROYO', 'VENDEDOR', 7698, '1990-02-20', 1500, 390, 30);
@@ -34,31 +36,31 @@ INSERT INTO EMPLE VALUES (7902, 'FERNANDEZ', 'ANALISTA', 7566, '1991-12-03', 300
 INSERT INTO EMPLE VALUES (7934, 'MUÑOZ', 'EMPLEADO', 7782, '1992-01-23', 1690, NULL, 10);
 
 create table ALUM0405(
-DNI	  varchar(10) PRIMARY KEY,
-NOMBRE	  varchar(15) NOT NULL,
-APELLIDOS varchar(20) NOT NULL,
-FECHA_NAC DATE,
-DIRECCION varchar(20),
-POBLACION varchar(20),
-PROVINCIA varchar(20),
-CURSO	  numeric(2)  NOT NULL,
-NIVEL	  varchar(10)  NOT NULL,
-CLASE	  CHAR(1)  NOT NULL,
-FALTAS1	  numeric(2),
-FALTAS2	  numeric(2),
-FALTAS3	  numeric(2)
+	DNI	  varchar(10) PRIMARY KEY,
+	NOMBRE	  varchar(15) NOT NULL,
+	APELLIDOS varchar(20) NOT NULL,
+	FECHA_NAC DATE,
+	DIRECCION varchar(20),
+	POBLACION varchar(20),
+	PROVINCIA varchar(20),
+	CURSO	  numeric(2)  NOT NULL,
+	NIVEL	  varchar(10)  NOT NULL,
+	CLASE	  CHAR(1)  NOT NULL,
+	FALTAS1	  numeric(2),
+	FALTAS2	  numeric(2),
+	FALTAS3	  numeric(2)
 );
 
 INSERT INTO ALUM0405 VALUES ('34448974N', 'CONCHA', 'ESTRUCH VIDAL','1985-11-10','C/ MEDICO FELIX, 12','BERROCALEJO', 'CACERES',2,'DAI','A',0,0,0);
 INSERT INTO ALUM0405 VALUES ('34448999N', 'DORA', 'MARTIN BRE A','1994-11-10','C/ MEDICO FELIX, 10','BERROCALEJO', 'CACERES',1,'ESO','C',0,0,0);
 
 create table notas_alumnos
- (
+(
    NOMBRE_ALUMNO varchar(25) PRIMARY KEY,
    nota1         numeric(2),
    nota2         numeric(2),
    nota3         numeric(2)
- ) ;
+);
  
 insert into NOTAS_ALUMNOS VALUES ('Alcalde García, M. Luisa', 5, 5, 5);
 insert into NOTAS_ALUMNOS VALUES ('Benito Martín, Luis', 7, 6, 8);
@@ -67,10 +69,11 @@ insert into NOTAS_ALUMNOS VALUES ('Corregidor Sánchez, Ana', 6, 9, 8);
 insert into NOTAS_ALUMNOS VALUES ('Díaz Sánchez, Maria', NULL, NULL, 7);
 
 create table LIBRERIA
- (TEMA       CHAR(15) NOT NULL ,
-  ESTANTE    CHAR(1),
-  EJEMPLARES numeric(2)
- ) ;
+(
+	TEMA       CHAR(15) NOT NULL ,
+	ESTANTE    CHAR(1),
+	EJEMPLARES numeric(2)
+);
 
 INSERT INTO LIBRERIA VALUES ('INFORMATICA', 'A', 5);
 INSERT INTO LIBRERIA VALUES ('ECONOMIA',    'A', 10);
@@ -86,24 +89,24 @@ INSERT INTO LIBRERIA VALUES ('JARDINERIA',  'E', 6);
 
 CREATE TABLE ALUMNOS
 (
-  DNI    varchar(10) PRIMARY KEY,
-  APENOM varchar(30),
-  DIREC  varchar(30),
-  POBLA  varchar(15),
-  TELEF  varchar(10)  
-) ;
+	  DNI    varchar(10) PRIMARY KEY,
+	  APENOM varchar(30),
+	  DIREC  varchar(30),
+	  POBLA  varchar(15),
+	  TELEF  varchar(10)  
+);
 
 CREATE TABLE MODULOS
 (
-  COD    numeric(2) PRIMARY KEY,
-  NOMBRE varchar(25)
-) ;
+	  COD    numeric(2) PRIMARY KEY,
+	  NOMBRE varchar(25)
+);
 
 CREATE TABLE NOTAS
 (
-  DNI varchar(10) NOT NULL,
-  COD numeric(2)  NOT NULL,
-  NOTA numeric(2)
+	  DNI varchar(10) NOT NULL,
+	  COD numeric(2)  NOT NULL,
+	  NOTA numeric(2)
 );
 
 INSERT INTO MODULOS VALUES (1,'Programación');
@@ -137,4 +140,4 @@ INSERT INTO NOTAS VALUES('56882942', 5, 7);
 INSERT INTO NOTAS VALUES('56882942', 6, 8);
 INSERT INTO NOTAS VALUES('56882942', 7, 9);
 
-CREATE TABLE auditaremple(operacion VARCHAR(200));
+CREATE TABLE auditaremple(operacion varchar(200));
