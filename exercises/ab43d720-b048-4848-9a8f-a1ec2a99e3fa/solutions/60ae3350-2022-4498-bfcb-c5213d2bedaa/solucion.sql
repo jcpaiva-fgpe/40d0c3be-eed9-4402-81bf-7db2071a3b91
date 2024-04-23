10999a577b7dbcdbaf61f14b2fa4fe715687e124
Andrew Nightingale <andrewpeter.nightingale@murciaeduca.es>
2024-04-23 11:05:27 +0100

    updated file exercises/ab43d720-b048-4848-9a8f-a1ec2a99e3fa/solutions/60ae3350-2022-4498-bfcb-c5213d2bedaa/solucion.sql

--- exercises/ab43d720-b048-4848-9a8f-a1ec2a99e3fa/solutions/60ae3350-2022-4498-bfcb-c5213d2bedaa/solucion.sql
+++ exercises/ab43d720-b048-4848-9a8f-a1ec2a99e3fa/solutions/60ae3350-2022-4498-bfcb-c5213d2bedaa/solucion.sql
@@ -8,19 +8,17 @@ DECLARE
 	nuevo_dept_id int;
 BEGIN
 	IF TG_OP = 'DELETE' THEN 
-		UPDATE EMPLE SET dept_no = -1 where dept_no = OLD.dept_no;
+	UPDATE EMPLE SET dept_no = -1 where dept_no = OLD.dept_no;
 	ELSIF TG_OP = 'INSERT' THEN
-		SELECT count(*) 
-        INTO existe_dept_nombre
-		from depart
-		WHERE dnombre = NEW.dnombre;
+	SELECT count(*) INTO existe_dept_nombre
+	WHERE nombre = OLD.dnombre;
 		IF existe_dept_nombre > 0 THEN
 			raise 'Ya existe un departamento con ese nombre';
 		ELSE
 			SELECT MAX(dept_no)+10
-			INTO nuevo_dept_id 
-			FROM depart;
-			NEW.dept_no := nuevo_dept_id;
+INTO nuevo_dept_id 
+FROM DEPARTAMENTOS;
+NEW.dept_no := nuevo_dept_id;
 		END IF;
 	END IF;
 	RETURN NEW;
