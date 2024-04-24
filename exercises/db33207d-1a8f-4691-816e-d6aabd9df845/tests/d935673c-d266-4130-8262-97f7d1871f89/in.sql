85a84987fffdea6ac8fea07fce2f6a05e71cafa1
Andrew Nightingale <andrewpeter.nightingale@murciaeduca.es>
2024-04-24 12:38:42 +0100

    updated file exercises/db33207d-1a8f-4691-816e-d6aabd9df845/tests/d935673c-d266-4130-8262-97f7d1871f89/in.sql

--- exercises/db33207d-1a8f-4691-816e-d6aabd9df845/tests/d935673c-d266-4130-8262-97f7d1871f89/in.sql
+++ exercises/db33207d-1a8f-4691-816e-d6aabd9df845/tests/d935673c-d266-4130-8262-97f7d1871f89/in.sql
@@ -1,2 +1,4 @@
 insert into locations(location_id, street_address, postal_code, city, state_province, country_id)
 values(1, 'Pasaje los Nietos', '30397', 'Cartagena', null, 'ES');
+
+select * from locations order by location_id;
