7f7e5118840dbb8f6ec702ae63d8bcec7cc63c0b
Andrew Nightingale <andrewpeter.nightingale@murciaeduca.es>
2024-04-24 12:48:40 +0100

    updated file exercises/cfa7969a-fcd3-452e-b8db-9fed8f795dca/solutions/7a9631f8-1e0d-4d5a-ab78-5b93146dda57/solucion.sql

--- exercises/cfa7969a-fcd3-452e-b8db-9fed8f795dca/solutions/7a9631f8-1e0d-4d5a-ab78-5b93146dda57/solucion.sql
+++ exercises/cfa7969a-fcd3-452e-b8db-9fed8f795dca/solutions/7a9631f8-1e0d-4d5a-ab78-5b93146dda57/solucion.sql
@@ -1,4 +1,4 @@
-CREATE or replace FUNCTION empleados_antiguedad_1990()
+CREATE or replace FUNCTION empleados_antiguedad_1995()
 RETURNS TRIGGER
 LANGUAGE plpgsql
 AS $$
@@ -22,4 +22,4 @@ CREATE TRIGGER eliminar_empleado
 BEFORE DELETE
 ON employees
 FOR EACH ROW
-EXECUTE PROCEDURE empleados_antiguedad_1990();
+EXECUTE PROCEDURE empleados_antiguedad_1995();
