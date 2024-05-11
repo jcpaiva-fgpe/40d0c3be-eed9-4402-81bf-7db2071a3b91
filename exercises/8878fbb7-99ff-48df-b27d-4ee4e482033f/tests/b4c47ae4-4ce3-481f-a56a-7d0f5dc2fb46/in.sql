09af796fa640d9518e58730e51199b27eb255bb9
Andrew Nightingale <andrewpeter.nightingale@murciaeduca.es>
2024-05-11 17:42:37 +0100

    created file exercises/8878fbb7-99ff-48df-b27d-4ee4e482033f/tests/b4c47ae4-4ce3-481f-a56a-7d0f5dc2fb46/in.sql

---
+++ exercises/8878fbb7-99ff-48df-b27d-4ee4e482033f/tests/b4c47ae4-4ce3-481f-a56a-7d0f5dc2fb46/in.sql
@@ -0,0 +1,12 @@
+select obtener_juegos_jugados_juntos(1)
+union
+select obtener_juegos_jugados_juntos(2)
+union
+select obtener_juegos_jugados_juntos(3)
+union
+select obtener_juegos_jugados_juntos(4)
+union
+select obtener_juegos_jugados_juntos(5)
+union
+select obtener_juegos_jugados_juntos(6)
+order by 1;
