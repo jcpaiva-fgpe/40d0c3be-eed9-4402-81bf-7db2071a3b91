076ddee9dce89010f2870529ca950269c16223c9
Alberto Sierra <alberto.sierra@murciaeduca.es>
2025-05-09 07:48:17 +0100

    updated file exercises/db33207d-1a8f-4691-816e-d6aabd9df845/tests/d935673c-d266-4130-8262-97f7d1871f89/in.sql

--- exercises/db33207d-1a8f-4691-816e-d6aabd9df845/tests/d935673c-d266-4130-8262-97f7d1871f89/in.sql
+++ exercises/db33207d-1a8f-4691-816e-d6aabd9df845/tests/d935673c-d266-4130-8262-97f7d1871f89/in.sql
@@ -1,7 +1,7 @@
-INSERT INTO public.countries (country_id,country_name,region_id)
+INSERT INTO countries (country_id,country_name,region_id)
 	VALUES ('ES','Spain',1);
 
-insert into locations(location_id, street_address, postal_code, city, state_province, country_id)
-values(1, 'Pasaje los Nietos', '30397', 'Cartagena', null, 'ES');
+INSERT INTO locations(location_id, street_address, postal_code, city, state_province, country_id)
+	VALUES (1, 'Pasaje los Nietos', '30397', 'Cartagena', null, 'ES');
 
 select * from locations order by location_id;
